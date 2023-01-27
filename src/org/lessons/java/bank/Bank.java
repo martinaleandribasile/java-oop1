34241ee63661e8734796f5523e5fbe8817c16801
MartinaBasile <martinaleandribasile@gmail.com>
2023-01-27 15:38:42 +0100

    operazione tipo stringa per poter inviare messaggio d errore con l inserimento di lettere

--- src/org/lessons/java/bank/Bank.java
+++ src/org/lessons/java/bank/Bank.java
@@ -7,7 +7,7 @@ public class Bank {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String nomeUtente;
-		int operazione=0;
+		String operazione="";
 		
 		System.out.println("Inserisci il tuo nome per l'apertura del conto:");
 		nomeUtente=s.nextLine().trim();
@@ -16,13 +16,13 @@ public class Bank {
 		
 		operazione= operazioni(operazione);
 		
-		while(operazione != 3) {
-			if(operazione == 1 ) {
+		while(!operazione.equals("3")) {
+			if(operazione.equals("1") ) {
 				System.out.println("Inserisca la somma da depositare (numeri decimali divisi dalla ,)");
 				double deposito= s.nextDouble();
 				newConto.deposita(deposito);
 				operazione= operazioni(operazione);
-			}else if(operazione == 2) {
+			}else if(operazione.equals("2")) {
 				System.out.println("Inserisca la somma da ritirare (numeri decimali divisi dalla ,)");
 				double ritiro = s.nextDouble();
 				newConto.ritira(ritiro);
@@ -35,10 +35,10 @@ public class Bank {
 		System.out.println("Grazie a arrivederci!");
 			
 	}
-	 public static int operazioni(int operazione) {
+	 public static String operazioni(String operazione) {
 		 Scanner s = new Scanner(System.in);
 		 System.out.println("Per VERSARE denaro digiti 1, per PRELEVARE denaro digiti 2, per USCIRE dal suo conto digiti 3");
-		 operazione= s.nextInt();
+		 operazione= s.nextLine();
 		 return operazione;
 	 }
 }

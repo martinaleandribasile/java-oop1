8584b2f319c5eff351ecd4a7101ddbbf480f8ee7
MartinaBasile <martinaleandribasile@gmail.com>
2023-01-27 16:10:45 +0100

    if per valutare il saldo e la possibilita' di prelevare senza rendere il saldo negativo

--- src/org/lessons/java/bank/Bank.java
+++ src/org/lessons/java/bank/Bank.java
@@ -1,9 +1,10 @@
 package org.lessons.java.bank;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Bank {
-
+	private static final DecimalFormat round = new DecimalFormat("0.00");
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String nomeUtente;
@@ -12,7 +13,10 @@ public class Bank {
 		System.out.println("Inserisci il tuo nome per l'apertura del conto:");
 		nomeUtente=s.nextLine().trim();
 		Conto newConto = new Conto(nomeUtente);
-		
+		System.out.println("Lei ha appena aperto il suo conto, ecco i dati:");
+		System.out.println("Nominativo: " + newConto.nomeProprietario);
+		System.out.println("Numero del Conto: " + newConto.numeroConto);
+		System.out.println("Saldo: " + round.format(newConto.saldo) + " euro");
 		
 		operazione= operazioni(operazione);
 		
@@ -21,12 +25,24 @@ public class Bank {
 				System.out.println("Inserisca la somma da depositare (numeri decimali divisi dalla ,)");
 				double deposito= s.nextDouble();
 				newConto.deposita(deposito);
+				System.out.println("Ora il suo saldo e' di : " + newConto.saldo + " euro");
 				operazione= operazioni(operazione);
 			}else if(operazione.equals("2")) {
-				System.out.println("Inserisca la somma da ritirare (numeri decimali divisi dalla ,)");
-				double ritiro = s.nextDouble();
-				newConto.ritira(ritiro);
-				operazione= operazioni(operazione);
+				if(newConto.saldo == 0) {
+					System.out.println("mi dispiace non e' possibile effettuare un ritiro , il suo conto non ha disponibilita'.");
+					operazione= operazioni(operazione);
+				}else {
+					System.out.println("Inserisca la somma da ritirare (numeri decimali divisi dalla ,)");
+					double ritiro = s.nextDouble();
+					if(newConto.saldo == 0 || ritiro > newConto.saldo) {
+						System.out.println("mi dispiace non e' possibile effettuare un ritiro , la disponibilita' non e'sufficiente");
+						operazione= operazioni(operazione);
+					}else {
+						newConto.ritira(ritiro);
+						 System.out.println("Ora il suo saldo e' di : " + newConto.saldo + " euro");
+						operazione= operazioni(operazione);
+					}
+				}
 			}else {
 				System.out.println("Mi dispiace la sua richiesta non e' valida riprovi:");
 				operazione= operazioni(operazione);
--- src/org/lessons/java/bank/Conto.java
+++ src/org/lessons/java/bank/Conto.java
@@ -1,10 +1,8 @@
 package org.lessons.java.bank;
 
-import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Conto {
-	private static final DecimalFormat round = new DecimalFormat("0.00");
 	Random r = new Random();
 	String nomeProprietario;
 	double saldo;
@@ -14,21 +12,15 @@ public class Conto {
 		this.nomeProprietario = nomeProprietario;
 		this.numeroConto= r.nextInt(1000);
 		this.saldo=0;
-		System.out.println("Lei ha appena aperto il suo conto, ecco i dati:");
-		System.out.println("Nominativo: " + this.nomeProprietario);
-		System.out.println("Numero del Conto: " + this.numeroConto);
-		System.out.println("Saldo: " + round.format(saldo) + " euro");
+		
 	}
 	 public double deposita(double value) {
 		 this.saldo += value;
-		 System.out.println("Ora il suo saldo e' di : " + this.saldo + " euro");
 		 return this.saldo;
 	 }
 	 
 	 public double ritira(double value) {
 		 this.saldo -= value;
-		 System.out.println("Ora il suo saldo e' di : " + this.saldo + " euro");
-		 if(this.saldo<0) System.out.println("Attenaione il conto e' in negativo, si consiglia di effettuare un deposito.");
 		 return this.saldo;
 	 }
 	
